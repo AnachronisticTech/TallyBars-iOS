18a066b88dfe819f2e2c593cf62f04e48ff5e4cf
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2020-10-02 23:27:50 +0100

    Initial Commit

---
+++ TallyBars/TallyBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyBarsApp.swift
+//  TallyBars
+//
+//  Created by Daniel Marriner on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TallyBarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
